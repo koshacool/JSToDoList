1aece200ab1079c64ecf1e1cc01590a9c3d5389a
kroman <roman.kushytskyy@gmail.com>
2018-11-03 20:02:22 +0200

    fix lint errors

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,3 @@
 export const generateId = () => Math.random().toString(36).substr(2, 9);
 
-export const capitalize = (str) => str.charAt(0).toUpperCase() + str.slice(1);
+export const capitalize = str => str.charAt(0).toUpperCase() + str.slice(1);
