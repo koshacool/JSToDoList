4df93d2c516ca1383c21d8b0ab5cf2ebb4260eae
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-11-11 16:47:04 +0000

    Deploy koshacool/ToDoList to github.com/koshacool/ToDoList.git:gh-pages

---
+++ precache-manifest.2d48057839d27f6c99820692fde56dc1.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce102cd98f650330ccba",
+    "url": "styles.css"
+  },
+  {
+    "revision": "02d1c20adb2dc4601a2e972f436e5626",
+    "url": "index.html"
+  },
+  {
+    "revision": "ce102cd98f650330ccba",
+    "url": "bundle.js"
+  }
+];
