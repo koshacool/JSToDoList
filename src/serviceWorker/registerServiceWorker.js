f11d86bedfed1472d8b36653382bb4b50be77ea4
roman <roman.kushytskyy@gmail.com>
2018-11-11 19:38:05 +0200

    change config

--- src/serviceWorker/registerServiceWorker.js
+++ src/serviceWorker/registerServiceWorker.js
@@ -2,10 +2,10 @@ export const registerServiceWorker = NODE_ENV => {
   if ('serviceWorker' in navigator) {
     if (NODE_ENV === 'development') {
       window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/ToDoList/sw.js');
+        navigator.serviceWorker.register('/service-worker.js');
       });
     } else {
-      navigator.serviceWorker.register('/ToDoList/sw.js');
+      navigator.serviceWorker.register('/ToDoList/service-worker.js');
     }
   }
 };
